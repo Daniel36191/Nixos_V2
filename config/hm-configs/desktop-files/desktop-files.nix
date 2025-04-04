292777e1ce7e14f51205d9a0028358d9307febc3
dmoeller131 <danielmoeller01@gmail.com>
2025-04-03 20:52:35 -0400

    windows

---
+++ config/hm-configs/desktop-files/desktop-files.nix
@@ -0,0 +1,29 @@
+{ config, ... }:
+
+{
+  xdg.desktopEntries.balatro = {
+    name = "Balatro";
+    exec = "love /home/daniel/Desktop/Games/Balatro_Love/Balatro.love";
+    terminal = false;
+    type = "Application";
+    categories = ["Application"];
+  };
+
+  xdg.desktopEntries.cardinal = {
+    name = "Cardinal";
+    exec = "Cardinal";
+    terminal = false;
+    type = "Application";
+    categories = ["Application"];
+  };
+
+  xdg.desktopEntries.windows = {
+    name = "Windows";
+    icon = "./windows.png";
+    exec = ''pkexec env "DISPLAY=\\$DISPLAY" "XAUTHORITY=\\$XAUTHORITY" sh -c "grub-reboot \\"Windows Boot Manager (on /dev/sdb1)\\"; reboot"'';
+    terminal = false;
+    type = "Application";
+    categories = ["Application"];
+  };
+
+}
