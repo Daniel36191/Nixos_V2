0428bea0fcae1fde09af98129d7eb769ad64c23f
Daniel36191 <dmoeller38@outlook.com>
2025-09-02 01:05:47 -0400

    laptop

--- config/hm-configs/hyprland/hyprland.nix
+++ config/hm-configs/hyprland/hyprland.nix
@@ -31,8 +31,8 @@
     systemd.enable = true;
     plugins = [
       # pkgs.hyprlandPlugins.hycov ## alt tab like function ## Broken in Nixpkgs
-      inputs.hyprsplit.packages.${pkgs.stdenv.hostPlatform.system}.hyprsplit
-    ];
+      
+    ] ++ (lib.optionals (nix-host == "pc") [ inputs.hyprsplit.packages.${pkgs.stdenv.hostPlatform.system}.hyprsplit ]);
     extraConfig =
       let
         hyprland-main = builtins.readFile ./hyprland-main.conf;
