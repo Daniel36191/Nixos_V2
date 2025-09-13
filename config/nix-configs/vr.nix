9bd87dca6a11eaee08768b452ea870de8be19a4b
Daniel36191 <dmoeller38@outlook.com>
2025-09-13 16:11:10 -0400

    things

--- config/nix-configs/vr.nix
+++ config/nix-configs/vr.nix
@@ -15,7 +15,10 @@
     inputs.lemonake.packages.${pkgs.system}.wlx-overlay-s
     inputs.lemonake.packages.${pkgs.system}.wayvr-dashboard
 
-    slimevr # # slime vr :)
+    # slimevr ## slime vr :)
+
+    ## Hotas remapping
+    input-remapper
 
   ];
 
