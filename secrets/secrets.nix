e5101a3d77eb9f2e560c1406a6d7517759fa7202
Daniel36191 <dmoeller38@outlook.com>
2025-09-25 22:07:55 -0400

    key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,10 +1,10 @@
 let
   ## Host public ssh key foubd by cat /etc/ssh/ssh_host_ed25519_key.pub
   pc = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILnEroABBEofeQFFpUxifyT8RUg5g6KmtJV8bW6zV2NA";
-  # laptop = "ssh-ed25519 AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+  laptop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOxMd0Bm0iX1OEXnnyRf/kF6eogn/zk3cPSfm23oMUwP";
   systems = [
     pc
-    # laptop
+    laptop
   ];
 in
 {
