18b85a00fa9ac37211bb23d8fda7b72b136ca169
Daniel36191 <dmoeller38@outlook.com>
2025-11-13 19:49:12 -0500

    dankshell pc

--- config/pc/hm-main-pc.nix
+++ config/pc/hm-main-pc.nix
@@ -1,5 +1,5 @@
 {
-  pkgs,
+  inputs,
   ...
 }:
 {
@@ -16,11 +16,14 @@
     ../hm-configs/app-configs/rofi/rofi.nix
     ../hm-configs/app-configs/rofi/config-emoji.nix
     ../hm-configs/app-configs/rofi/config-long.nix
-    ../hm-configs/app-configs/swaync.nix
-    ../hm-configs/app-configs/waybar.nix ##old waybar
+    # ../hm-configs/app-configs/swaync.nix
+    # ../hm-configs/app-configs/waybar.nix ##old waybar
     # ../hm-configs/app-configs/waybar/waybar.nix
     # ../hm-configs/app-configs/quickshell.nix
     # ../hm-configs/app-configs/hyprpanel/hyprpanel.nix
+    inputs.dms.homeModules.dankMaterialShell.default
+    ../hm-configs/app-configs/dankshell.nix
+
     ../hm-configs/app-configs/btop.nix
     ../hm-configs/app-configs/swappy.nix
     ../hm-configs/app-configs/github.nix
