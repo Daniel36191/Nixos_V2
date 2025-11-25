583be53ab367447a1db07daa9a6ac282f841dafe
Daniel36191 <dmoeller38@outlook.com>
2025-11-24 20:53:31 -0500

    proxmox

---
+++ config/nix-configs/proxmox.nix
@@ -0,0 +1,24 @@
+{
+  proxmoxOverlay,
+  ...
+}:
+{
+  services.proxmox-ve = {
+    enable = true;
+    ipAddress = "192.168.0.1";
+  };
+
+  ## Add Packages from flake overlay
+  nixpkgs.overlays = [ proxmoxOverlay ];
+
+  nix = {
+    settings = {
+      substituters = [
+        "https://cache.saumon.network/proxmox-nixos"
+      ];
+      trusted-public-keys = [
+        "proxmox-nixos:D9RYSWpQQC/msZUWphOY2I5RLH5Dd6yQcaHIuug7dWM="
+      ];
+    };
+  };
+}
