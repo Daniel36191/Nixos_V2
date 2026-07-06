c46f78eba875840b9826f705c50d102efcebb700
Daniel36191 <dmoeller3998@gmail.com>
2026-07-06 01:54:00 -0400

    Dissable HFP to fix bluetooth devices not connecting to the hfp backend

--- modules/nix/sound.nix
+++ modules/nix/sound.nix
@@ -70,14 +70,12 @@ in
       wireplumber = {
         enable = true;
         extraConfig = {
-          ## Take Calls On Pc (HFP)
           "monitor.bluez.properties" = {
             bluez5.roles = [
               "a2dp_sink"
               "a2dp_source"
               "bap_sink"
               "bap_source"
-              "hsp_hs"
             ];
             bluez5.enable-msbc = true;
             bluez5.hfphsp-backend = "native";
